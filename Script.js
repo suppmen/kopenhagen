6e8a1a4e8c81368e20cc5f43a262ac57b8861f54
suzana-melinda <zsuz0095@stud.kea.dk>
2020-04-28 21:38:48 +0200

    script-update

--- Script.js
+++ Script.js
@@ -1,11 +1,10 @@
+/*********** toggeling the burger and nav ************/
 
 window.addEventListener("load", setup);
 
 function setup() {
     setupBurgherNav();
-
 }
-
 
 function setupBurgherNav() {
     const burger = document.querySelector("header svg");
@@ -15,4 +14,55 @@ function setupBurgherNav() {
         nav.classList.toggle("open");
     });
 }
+/*******************************************************/
+
+
+
+
+/*********************** Get Data from WP **************************/
+
+const link0 = "http://mymmd.dk/Kopenhagen/wp-json/wp/v2/art_calendar?per_page=100";
+const link1 = "http://mymmd.dk/Kopenhagen/wp-json/wp/v2/art_calendar?per_page=100&_embed";
+window.addEventListener("DOMContentLoaded", getData);
+
+
+
+
+/***** fetch Data *****/
+
+function getData(){
+    fetch(link1)
+    .then(function(response){
+        return response.json();
+    })
+    .then(showData);
+}
+
+function showData(artArray){
+   console.log(artArray, "artArray");
+    artArray.forEach(art => {
+        console.log(art,"LoopTest");
+
+        const template = document.querySelector("template").content;
+
+        const copy = template.cloneNode(true);
+
+        copy.querySelector('.event-title').textContent = art.title.rendered;
+        copy.querySelector('.artCat').textContent = art._embedded["wp:term"][1][0].name;
+        copy.querySelector('.gallery-name').textContent = art.gallery_name;
+        copy.querySelector('.artist-name').textContent = art.artist_name;
+//        copy.querySelector('.event-adress').textContent = art._embedded["wp:term"][3][0].name;
+        copy.querySelector('.event-adress').textContent = art.address;
+
+
+
+
+        document.querySelector(".cards-wrapper").appendChild(copy);
+
+    });
+
+}
+
+/*********************************************************************************************************/
+
 
