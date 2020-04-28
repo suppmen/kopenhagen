67aaaf69635da016ac32e245483fc6f3bef72d5e
Dung Le <DungLe@Dungs-MacBook-Pro.local>
2020-04-28 09:12:11 +0200

    add header

---
+++ Script.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+=======
+window.addEventListener("load", setup);
+
+function setup() {
+    setupBurgherNav();
+
+}
+
+
+function setupBurgherNav() {
+    const burger = document.querySelector("header svg");
+    const nav = document.querySelector("nav")
+    burger.addEventListener("click", e => {
+        burger.classList.toggle("open");
+        nav.classList.toggle("open");
+    });
+}
+>>>>>>> origin/master
