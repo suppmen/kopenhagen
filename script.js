05ae395b67014eeda3e01917ef4344a2dd297dea
suppme <suppmen@gmail.com>
2020-05-01 09:02:34 +0200

    SinglePage+Js

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function setupBurgherNav() {
 
 const link0 = "http://mymmd.dk/Kopenhagen/wp-json/wp/v2/art_calendar?per_page=100";
 const link1 = "http://mymmd.dk/Kopenhagen/wp-json/wp/v2/art_calendar?per_page=100&_embed";
+
 window.addEventListener("DOMContentLoaded", getData);
 
 
@@ -31,14 +32,29 @@ window.addEventListener("DOMContentLoaded", getData);
 /***** fetch Data *****/
 
 function getData(){
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log('URLSearchParams' + window.location);
+    const the_art_id = urlParams.get('art_calendar_id');
+    const link2 = "http://mymmd.dk/Kopenhagen/wp-json/wp/v2/art_calendar/"+the_art_id+"?per_page=100&_embed";
+    console.log(the_art_id, "IdTest");
+
+    if (the_art_id){
+        fetch(link2)
+        .then(function(response){
+            return response.json()
+        })
+        .then(showSingleArtPage)
+    }else{
     fetch(link1)
     .then(function(response){
         return response.json();
     })
-    .then(showData);
+    .then(showArt_CalendarData);
+}
 }
 
-function showData(artArray){
+
+function showArt_CalendarData(artArray){
    console.log(artArray, "artArray");
     artArray.forEach(art => {
         console.log(art,"LoopTest");
@@ -56,12 +72,48 @@ function showData(artArray){
 //        copy.querySelector('img').src = art._embedded["wp:featuredmedia"][0].source_url;
         copy.querySelector("img").src = art._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
 
+        const a = copy.querySelector('a');
+
+        if (a){
+                    a.href += art.id;
+
+        }
+
 
         document.querySelector(".cards-wrapper").appendChild(copy);
 
     });
 
 }
+
+
+function showSingleArtPage(art){
+
+        const template = document.querySelector("template").content;
+
+        const copy = template.cloneNode(true);
+
+        const divArtDescription = copy.querySelector('#art-description');
+        console.log(divArtDescription, "div");
+
+    if(divArtDescription){
+        divArtDescription.innerHTML = art.content.rendered;
+        copy.querySelector('.event-title').textContent = art.title.rendered;
+        copy.querySelector('.artCat').textContent = art._embedded["wp:term"][1][0].name;
+        copy.querySelector('.gallery-name').textContent = art.gallery_name;
+        copy.querySelector('.artist-name').textContent = art.artist_name;
+//        copy.querySelector('.event-adress').textContent = art._embedded["wp:term"][3][0].name;
+        copy.querySelector('.event-adress').textContent = art.address;
+//        copy.querySelector('img').src = art._embedded["wp:featuredmedia"][0].source_url;
+        copy.querySelector("img").src = art._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+}
+
+
+        document.querySelector(".cards-wrapper").appendChild(copy);
+
+    };
+
+
 
 /*********************************************************************************************************/
 
